690cf533c76ca309448ef8496834b699a03992e7
Mich Seixas <mcseixas@gmail.com>
2023-09-14 19:09:44 +0200

    props children

---
+++ props-children-example/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Apples from './components/Apples'
+import Pears from './components/Pears'
+import Bags from './components/Bags'
+
+function App() {
+  return (
+    <div>
+      <Apples />
+      <Pears/>
+      <Bags/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ props-children-example/src/components/Bags.js
@@ -0,0 +1,17 @@
+function Bag(props) {
+    const bag = {
+        padding: "20px",
+        border: "1px solid gray",
+        background: "#fff",
+        margin: "20px 0"
+    }
+    return (
+        <div style={bag}>
+            {props.children}
+        </div>
+    )
+}
+export default Bag
+
+// the Bag component adds a wrapping div with a specific styling, 
+// and then gives it props.children as its content.
