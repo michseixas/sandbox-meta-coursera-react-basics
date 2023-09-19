7a5c46cf5c3ad3f42d0b59b8f3b88586fdffd0d4
Mich Seixas <mcseixas@gmail.com>
2023-09-19 19:21:54 +0200

    event handler

--- multiple-components/src/App.js
+++ multiple-components/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Card from './component/Card';
+import Btn from "./component/Btn";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Card h2="First card's h2" h3="First card's h3"/>
       <Card h2="Second card's h2" h3="Second card's h3"/>
       <Card h2="Third card's h2" h3="Third card's h3"/>
+      <Btn />
     </div>
   );
 }
